6ef0220f760709fe7ab36f8856e31cdf524adc20
Matan Goldfarb <matangoldfarb1@gmail.com>
2024-02-13 09:59:15 +0200

    first commit of the project

---
+++ src/main/java/bguspl/set/ex/Dealer.java
@@ -0,0 +1,180 @@
+package bguspl.set.ex;
+
+import bguspl.set.Env;
+
+import java.text.CollationKey;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * This class manages the dealer's threads and data
+ */
+public class Dealer implements Runnable {
+
+    /**
+     * The game environment object.
+     */
+    private final Env env;
+
+    /**
+     * Game entities.
+     */
+    private final Table table;
+    private final Player[] players;
+
+    /**
+     * The list of card ids that are left in the dealer's deck.
+     */
+    private final List<Integer> deck;
+
+    /**
+     * True iff game should be terminated.
+     */
+    private volatile boolean terminate;
+
+    /**
+     * The time when the dealer needs to reshuffle the deck due to turn timeout.
+     */
+    private long reshuffleTime = Long.MAX_VALUE;
+
+    private boolean notify;
+
+    public Dealer(Env env, Table table, Player[] players) {
+        this.env = env;
+        this.table = table;
+        this.players = players;
+        deck = IntStream.range(0, env.config.deckSize).boxed().collect(Collectors.toList());
+        this.notify = false;
+    }
+
+    /**
+     * The dealer thread starts here (main loop for the dealer thread).
+     */
+    @Override
+    public void run() {
+        env.logger.info("thread " + Thread.currentThread().getName() + " starting.");
+        for(Player player : players){
+            player.createThread();
+        }
+        while (!shouldFinish()) {
+            Collections.shuffle(deck);
+            placeCardsOnTable();
+            timerLoop();
+            updateTimerDisplay(false);
+            removeAllCardsFromTable();
+        }
+        announceWinners();
+        env.logger.info("thread " + Thread.currentThread().getName() + " terminated.");
+    }
+
+    /**
+     * The inner loop of the dealer thread that runs as long as the countdown did not time out.
+     */
+    private void timerLoop() {
+        while (!terminate && System.currentTimeMillis() < reshuffleTime) {
+            sleepUntilWokenOrTimeout();
+            updateTimerDisplay(false);
+            removeCardsFromTable();
+            placeCardsOnTable();
+        }
+    }
+
+    /**
+     * Called when the game should be terminated.
+     */
+    public void terminate() {
+        // TODO implement
+        for(Player player : players){
+            player.terminate();
+        }
+        this.terminate = true;
+    }
+
+    /**
+     * Check if the game should be terminated or the game end conditions are met.
+     *
+     * @return true iff the game should be finished.
+     */
+    private boolean shouldFinish() {
+        return terminate || env.util.findSets(deck, 1).size() == 0;
+    }
+
+    /**
+     * Checks cards should be removed from the table and removes them.
+     */
+    private void removeCardsFromTable() {
+        // TODO implement
+    }
+
+    /**
+     * Check if any cards can be removed from the deck and placed on the table.
+     */
+    private void placeCardsOnTable() {
+        boolean placed = false;
+        if(!deck.isEmpty()){
+            for(int i=0 ; i<(table.slotToCard).length ; i++){
+                if(!deck.isEmpty() && table.slotToCard[i] == null){
+                    Integer card = deck.get(0);
+                    deck.remove(0);
+                    table.placeCard(card, i);
+                    placed = true;
+                }
+            }
+        }
+        if(placed){
+            updateTimerDisplay(true);
+        }
+    }
+
+    /**
+     * Sleep for a fixed amount of time or until the thread is awakened for some purpose.
+     */
+    private synchronized void sleepUntilWokenOrTimeout() {
+        // TODO implement
+        try {
+            this.wait(1000);
+        } catch (InterruptedException ignored) {}
+        if(this.notify){
+            //check and act
+        }
+        this.notify = false;
+    }
+
+    /**
+     * Reset and/or update the countdown and the countdown display.
+     */
+    private void updateTimerDisplay(boolean reset) {
+        if(reset){
+            reshuffleTime = System.currentTimeMillis() + env.config.turnTimeoutMillis;
+        }
+        env.ui.setCountdown(reshuffleTime-System.currentTimeMillis(), false);
+    }
+
+    /**
+     * Returns all the cards from the table to the deck.
+     */
+    private void removeAllCardsFromTable() {
+        for(int i=0 ; i<(table.slotToCard).length ; i++){
+            if(table.slotToCard[i] != null){
+                Integer card = table.slotToCard[i];
+                deck.add(card);
+                table.removeCard(i);
+            }
+        }
+    }
+
+    /**
+     * Check who is/are the winner/s and displays them.
+     */
+    private void announceWinners() {
+        // TODO implement
+    }
+
+    public void notifyThread() {
+        this.notify = true;
+        notify();
+    }
+}
---
+++ src/main/java/bguspl/set/ex/Player.java
@@ -0,0 +1,195 @@
+package bguspl.set.ex;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.Iterator;
+
+import bguspl.set.Env;
+
+/**
+ * This class manages the players' threads and data
+ *
+ * @inv id >= 0
+ * @inv score >= 0
+ */
+public class Player implements Runnable {
+
+    /**
+     * The game environment object.
+     */
+    private final Env env;
+
+    /**
+     * Game entities.
+     */
+    private final Table table;
+
+    /**
+     * The id of the player (starting from 0).
+     */
+    public final int id;
+
+    /**
+     * The thread representing the current player.
+     */
+    private Thread playerThread;
+
+    /**
+     * The thread of the AI (computer) player (an additional thread used to generate key presses).
+     */
+    private Thread aiThread;
+
+    /**
+     * True iff the player is human (not a computer player).
+     */
+    private final boolean human;
+
+    /**
+     * True iff game should be terminated.
+     */
+    private volatile boolean terminate;
+
+    /**
+     * The current score of the player.
+     */
+    private int score;
+
+    private Dealer dealer;
+
+    private final List<Integer> tokens;
+    private Object lock = new Object();
+    private BlockingQueue<Integer> actionsQueue;
+
+    /**
+     * The class constructor.
+     *
+     * @param env    - the environment object.
+     * @param dealer - the dealer object.
+     * @param table  - the table object.
+     * @param id     - the id of the player.
+     * @param human  - true iff the player is a human player (i.e. input is provided manually, via the keyboard).
+     */
+    public Player(Env env, Dealer dealer, Table table, int id, boolean human) {
+        this.env = env;
+        this.table = table;
+        this.id = id;
+        this.human = human;
+        this.dealer = dealer;
+        this.tokens = new ArrayList<>();
+        this.playerThread = new Thread(this);
+        this.actionsQueue = new ArrayBlockingQueue<>(3);
+    }
+
+    /**
+     * The main player thread of each player starts here (main loop for the player thread).
+     */
+    @Override
+    public void run() {
+        playerThread = Thread.currentThread();
+        env.logger.info("thread " + Thread.currentThread().getName() + " starting.");
+        if (!human) createArtificialIntelligence();
+
+        while (!terminate) {
+            // TODO implement main player loop
+            try {
+                Integer slot = this.actionsQueue.take();
+                if(this.tokens.contains(slot)){
+                    this.tokens.remove(slot);
+                    table.removeToken(id, slot);
+                }
+                else{
+                    this.tokens.add(slot);
+                    this.table.placeToken(id, slot);
+                }
+                if(this.tokens.size() == 3){
+                    //tell dealer to check
+
+                    // delete all tokens
+                    Iterator<Integer> iterator = this.tokens.iterator();
+                    while (iterator.hasNext()) {
+                        Integer element = iterator.next();
+                        iterator.remove();
+                        table.removeToken(id, element);
+                    }
+                }
+            } catch (InterruptedException ignored) {}
+        }
+        if (!human) try { aiThread.join(); } catch (InterruptedException ignored) {}
+        env.logger.info("thread " + Thread.currentThread().getName() + " terminated.");
+    }
+
+    /**
+     * Creates an additional thread for an AI (computer) player. The main loop of this thread repeatedly generates
+     * key presses. If the queue of key presses is full, the thread waits until it is not full.
+     */
+    private void createArtificialIntelligence() {
+        // note: this is a very, very smart AI (!)
+        aiThread = new Thread(() -> {
+            env.logger.info("thread " + Thread.currentThread().getName() + " starting.");
+            while (!terminate) {
+                // TODO implement player key press simulator
+                try {
+                    synchronized (this) { wait(); }
+                } catch (InterruptedException ignored) {}
+            }
+            env.logger.info("thread " + Thread.currentThread().getName() + " terminated.");
+        }, "computer-" + id);
+        aiThread.start();
+    }
+
+    /**
+     * Called when the game should be terminated.
+     */
+    public void terminate() {
+        // TODO implement
+        this.terminate=true;
+    }
+
+    /**
+     * This method is called when a key is pressed.
+     *
+     * @param slot - the slot corresponding to the key pressed.
+     */
+    public void keyPressed(int slot) {
+        // TODO implement
+        try {
+            actionsQueue.put(slot);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    /**
+     * Award a point to a player and perform other related actions.
+     *
+     * @post - the player's score is increased by 1.
+     * @post - the player's score is updated in the ui.
+     */
+    public void point() {
+        // TODO implement
+
+        int ignored = table.countCards(); // this part is just for demonstration in the unit tests
+        env.ui.setScore(id, ++score);
+    }
+
+    /**
+     * Penalize a player and perform other related actions.
+     */
+    public void penalty() {
+        // TODO implement
+    }
+
+    public int score() {
+        return score;
+    }
+
+    public void createThread() {
+        this.playerThread = new Thread(this);
+        this.playerThread.start();
+    }
+}
